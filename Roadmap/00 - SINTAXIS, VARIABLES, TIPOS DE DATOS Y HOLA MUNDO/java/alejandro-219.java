eb8e2a73a678ebd8f8befd3c27027dc75359c08b
alejandrox-219 <nahuel.219a@gmail.com>
2024-10-10 20:44:51 -0300

    Create alejandro-219.java

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/java/alejandro-219.java
@@ -0,0 +1,36 @@
+/* 1)Crea un comentario en el código y coloca la URL del sitio web oficial del
+    lenguaje de programación que has seleccionado.
+    
+    Sitio web oficial de Java: https://www.java.com */
+
+/* 2) Representa las diferentes sintaxis que existen de crear comentarios
+   en el lenguaje (en una línea, varias...).*/
+
+     //linea de comentario
+
+     /*   varias lineas
+              de 
+          comentarios */
+
+// 3) Crea una variable (y una constante si el lenguaje lo soporta).
+public class alejandrox-219{
+  public static void main(string[] args){
+   
+    int variable=15;
+    final int CONSTANTE= 22;
+/* 4) Crea variables representando todos los tipos de datos primitivos
+      del lenguaje (cadenas de texto, enteros, booleanos...).*/
+    int= 54;
+    float= 20.1;
+    double=10;
+    boulean= false;
+    char= "A";
+    string="texto";
+
+//  5)Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
+      
+   System.out.println("Hola,¡Java!");
+    
+  }
+  
+}
